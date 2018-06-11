3b3f44fe47ee6b4bb49a00cee9961d1bc2cacd3e
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2018-06-11 15:42:47 -0300

    Update manutencao_linux.sh

--- manutencao_linux.sh
+++ manutencao_linux.sh
@@ -264,7 +264,7 @@ case $opcao in
 	34) #VS Code
 		echo "Não possui instalação";;
 	
-	35) #Nodejs
+	35) #Node
 		sudo apt install curl
 		curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 		export NVM_DIR="$HOME/.nvm"
