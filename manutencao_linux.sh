427419dab069ac7e47ccb2ce41cdd9a385381d0a
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2018-06-11 15:37:40 -0300

    Update manutencao_linux.sh

--- manutencao_linux.sh
+++ manutencao_linux.sh
@@ -268,7 +268,8 @@ case $opcao in
 		sudo apt install curl
 		curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 		export NVM_DIR="$HOME/.nvm"
-		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";;
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+		nvm install node;;
 
 	36) #Composer
 		curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
